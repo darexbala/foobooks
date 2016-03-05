a0f0c1f8a178c31d6459b940f4bf85e623473809
Damilare Oladosu <darexbala@yahoo.com>
2016-03-04 19:03:50 -0500

    update app config

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,4 +9,9 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('/book/create', 'BookController@getCreate');
     Route::post('/book/create', 'BookController@postCreate');
     Route::get('/book/{id}', 'BookController@getShow');
+
+    Route::get('/practice', function(){
+        echo config('app.url');
+        return '';
+    });
 });
